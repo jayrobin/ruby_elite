66a16057e245189c8afaf5262675e0b11e66edf3
James Robinson <james.michael.robinson@googlemail.com>
2014-04-03 18:44:28 +0100

    fix command result output

--- lib/game.rb
+++ lib/game.rb
@@ -6,17 +6,11 @@ require_relative 'trade_good'
 class Game
 	attr_reader :running
 
-	DEFAULT_SEED = Seed.new(0x5A4A, 0x0248, 0xB753)
-	POLITICALLY_CORRECT = false
-	UNIT_T = "t"
-	UNIT_KG = "kg"
-	UNIT_G = "g"
-
 	def initialize
-		commodities = load_commodities
+		commodities = load_commodities(false)
 
 		@galaxy = Galaxy.new(1, commodities)
-		@galaxy.generate(DEFAULT_SEED.dup)
+		@galaxy.generate(Seed.new(0x5A4A, 0x0248, 0xB753))
 
 		@player = Player.new(@galaxy.systems[7], commodities.size)
 		@running = true
@@ -65,16 +59,16 @@ class Game
 
 	def command_buy(item_name, amount = 0)
 		amount = @player.buy(item_name, amount.to_i)
-		return "Could not complete trade" if amount == 0
+		return "Could not complete trade\n" if amount == 0
 
-		"Purchased #{amount} of #{item_name}"
+		"Purchased #{amount} of #{item_name}\n"
 	end
 
 	def command_sell(item_name, amount = 0)
 		amount = @player.sell(item_name, amount.to_i)
-		return "Could not complete trade" if amount == 0
+		return "Could not complete trade\n" if amount == 0
 
-		"Sold #{amount} of #{item_name}"
+		"Sold #{amount} of #{item_name}\n"
 	end
 
 	def command_get_local
@@ -101,39 +95,39 @@ class Game
 	end
 
 	def command_jump(planet_name, sneak = false)
-		return "Must enter a planet name" if planet_name.nil?
+		return "Must enter a planet name\n" if planet_name.nil?
 
 		planet = @galaxy.get_planet(planet_name.upcase)
-		return "Could not jump to #{planet_name}" if planet.nil? || planet == @player.planet
+		return "Could not jump to #{planet_name}\n" if planet.nil? || planet == @player.planet
 
-		@player.jump_to(planet, sneak) ? planet.print(false) : "Not enough fuel"
+		@player.jump_to(planet, sneak) ? planet.print(false) : "Not enough fuel\n"
 	end
 
 	def command_galhyp
 		current_planet_index = @galaxy.get_planet_index(@player.planet)
-		@galaxy.next(DEFAULT_SEED.dup)
+		@galaxy.next(Seed.new(0x5A4A, 0x0248, 0xB753))
 		@player.jump_to(@galaxy.systems[current_planet_index], true)
 
-		"Galaxy jump complete, now on #{@player.planet.name}"
+		"Galaxy jump complete, now on #{@player.planet.name}\n"
 	end
 
 	def command_fuel(amount)
-		return "Must supply an amount" if amount.nil?
+		return "Must supply an amount\n" if amount.nil?
 
 		fuel = @player.buy_fuel(amount.to_f)
-		format("You have %.1f LY of fuel", fuel)
+		format("You have %.1f LY of fuel\n", fuel)
 	end
 
 	def command_cash(amount)
 		return "Must supply an amount" if amount.nil?
 
-		"Cash set to $#{@player.cash = amount.to_f.abs}"
+		"Cash set to $#{@player.cash = amount.to_f.abs}\n"
 	end
 
 	def command_hold(amount)
 		return "Must supply an amount" if amount.nil?
 
-		"Hold set to #{@player.cargo_space = amount.to_f.abs}"
+		"Hold set to #{@player.cargo_space = amount.to_f.abs}\n"
 	end
 
 	def command_help
@@ -152,36 +146,40 @@ class Game
     output << "\nQuit or ^C       (exit)"
     output << "\nHelp             (display this text)"
     output << "\nRand             (toggle RNG)"
-    output << "\n\nAbbreviations allowed eg. b fo 5 = Buy Food 5, m= Mkt"
+    output << "\n\nAbbreviations allowed eg. b fo 5 = Buy Food 5, m= Mkt\n"
 	end
 
 	def command_exit
 		@running = false
-		"Quitting game..."
+		"Quitting game...\n"
 	end
 
 	def unknown_command(command)
-		"Unrecognised command: #{command}"
+		"Unrecognised command: #{command}\n"
 	end
 
-	def load_commodities
+	def load_commodities(politically_correct)
+		unit_t = "t"
+		unit_kg = "kg"
+		unit_g = "g"
+
 		@commodities = []
-		@commodities << TradeGood.new(0x13, -0x02, 0x06, 0x01, UNIT_T, "Food        ")
-    @commodities << TradeGood.new(0x14, -0x01, 0x0A, 0x03, UNIT_T, "Textiles    ")
-    @commodities << TradeGood.new(0x41, -0x03, 0x02, 0x07, UNIT_T, "Radioactives")
-    @commodities << TradeGood.new(0x28, -0x05, 0xE2, 0x1F, UNIT_T, POLITICALLY_CORRECT ? "Robot_Slaves" : "Slaves      ")
-    @commodities << TradeGood.new(0x53, -0x05, 0xFB, 0x0F, UNIT_T, POLITICALLY_CORRECT ? "Beverages   " : "Liquor/Wines")
-    @commodities << TradeGood.new(0xC4, +0x08, 0x36, 0x03, UNIT_T, "Luxuries    ")
-    @commodities << TradeGood.new(0xEB, +0x1D, 0x08, 0x78, UNIT_T, POLITICALLY_CORRECT ? "Rare_Species" : "Narcotics   ")
-    @commodities << TradeGood.new(0x9A, +0x0E, 0x38, 0x03, UNIT_T, "Computers   ")
-    @commodities << TradeGood.new(0x75, +0x06, 0x28, 0x07, UNIT_T, "Machinery   ")
-    @commodities << TradeGood.new(0x4E, +0x01, 0x11, 0x1F, UNIT_T, "Alloys      ")
-    @commodities << TradeGood.new(0x7C, +0x0d, 0x1D, 0x07, UNIT_T, "Firearms    ")
-    @commodities << TradeGood.new(0xB0, -0x09, 0xDC, 0x3F, UNIT_T, "Furs        ")
-    @commodities << TradeGood.new(0x20, -0x01, 0x35, 0x03, UNIT_T, "Minerals    ")
-    @commodities << TradeGood.new(0x61, -0x01, 0x42, 0x07, UNIT_KG, "Gold        ")
-    @commodities << TradeGood.new(0xAB, -0x02, 0x37, 0x1F, UNIT_KG, "Platinum    ")
-    @commodities << TradeGood.new(0x2D, -0x01, 0xFA, 0x0F, UNIT_G, "Gem_Stones ")
-    @commodities << TradeGood.new(0x35, +0x0F, 0xC0, 0x07, UNIT_T, "Alien_Items ")
+		@commodities << TradeGood.new(0x13, -0x02, 0x06, 0x01, unit_t, "Food        ")
+    @commodities << TradeGood.new(0x14, -0x01, 0x0A, 0x03, unit_t, "Textiles    ")
+    @commodities << TradeGood.new(0x41, -0x03, 0x02, 0x07, unit_t, "Radioactives")
+    @commodities << TradeGood.new(0x28, -0x05, 0xE2, 0x1F, unit_t, politically_correct ? "Robot_Slaves" : "Slaves      ")
+    @commodities << TradeGood.new(0x53, -0x05, 0xFB, 0x0F, unit_t, politically_correct ? "Beverages   " : "Liquor/Wines")
+    @commodities << TradeGood.new(0xC4, +0x08, 0x36, 0x03, unit_t, "Luxuries    ")
+    @commodities << TradeGood.new(0xEB, +0x1D, 0x08, 0x78, unit_t, politically_correct ? "Rare_Species" : "Narcotics   ")
+    @commodities << TradeGood.new(0x9A, +0x0E, 0x38, 0x03, unit_t, "Computers   ")
+    @commodities << TradeGood.new(0x75, +0x06, 0x28, 0x07, unit_t, "Machinery   ")
+    @commodities << TradeGood.new(0x4E, +0x01, 0x11, 0x1F, unit_t, "Alloys      ")
+    @commodities << TradeGood.new(0x7C, +0x0d, 0x1D, 0x07, unit_t, "Firearms    ")
+    @commodities << TradeGood.new(0xB0, -0x09, 0xDC, 0x3F, unit_t, "Furs        ")
+    @commodities << TradeGood.new(0x20, -0x01, 0x35, 0x03, unit_t, "Minerals    ")
+    @commodities << TradeGood.new(0x61, -0x01, 0x42, 0x07, unit_kg, "Gold        ")
+    @commodities << TradeGood.new(0xAB, -0x02, 0x37, 0x1F, unit_kg, "Platinum    ")
+    @commodities << TradeGood.new(0x2D, -0x01, 0xFA, 0x0F, unit_g, "Gem_Stones ")
+    @commodities << TradeGood.new(0x35, +0x0F, 0xC0, 0x07, unit_t, "Alien_Items ")
 	end
 end
